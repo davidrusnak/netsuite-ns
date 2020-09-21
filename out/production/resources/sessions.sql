60c9dff882679c0d83ba0a798165a39abb786045
Svec, Michal <msvec@netsuite.com>
2020-09-21 12:26:56 +0200

    cleanup of deleted files

--- out/production/resources/sessions.sql
+++
@@ -1,7 +0,0 @@
--- DDL of SQL table used in old service implementation
-CREATE TABLE Session
-(
-  "sId"      VARCHAR(255) PRIMARY KEY,
-  "sEmail"   VARCHAR(255) NOT NULL,
-  "dCreated" TIMESTAMP    NOT NULL
-);
